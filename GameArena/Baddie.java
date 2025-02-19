023f2661a3db672fd7f5e1afabe9f77d34450ccb
Pk13Dev <74469236+Pk13Dev@users.noreply.github.com>
2025-02-19 11:11:06 +0000

    Initial Commit

---
+++ GameArena/Baddie.java
@@ -0,0 +1,7 @@
+public class Baddie {
+    // Priv instance for X and Y
+    // Priv instance for shapes that make the character (Array)
+    // Constructor that take X and Y
+    // Public method "addTo" with param GameArena (spawn the character)
+    // Public method "move" 
+}
---
+++ GameArena/Goodie.java
@@ -0,0 +1,8 @@
+public class Goodie {
+    // Priv instance for X and Y
+    // Priv instance for shapes that make the character (Array)
+    // Constructor that take X and Y
+    // Public method "addTo" with param GameArena (spawn the character)
+    // Public method "move" 
+
+}
